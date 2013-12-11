6880191147e27a3fb33e9afe977cbeee9477d1b0
coleman ophelan <coleman.ophelan@gmail.com>
2013-12-11 17:36:52 -0500

    fix top nav html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 NewAuthDemo::Application.routes.draw do
 
   resources :users, only: [:create, :new, :show]
+
   resource :session, only: [:create, :destroy, :new]
 
   resources :resources, only: [:create, :show, :index]
+
   resources :sections, only: [:create, :show, :index]
 
   resources :notes, only: [:create, :show, :index]
