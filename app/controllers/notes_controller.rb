c3f5cef1bdbd699ed3879506cd256d4f9fffa67d
coleman ophelan <coleman.ophelan@gmail.com>
2013-12-11 13:20:13 -0500

    finish basic notes controller and add it to routes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,15 +6,27 @@ class NotesController < ApplicationController
     note = Note.new(params[:note])
     note.user_id = current_user.id
 
-
+    if note.save
+      render json: note
+    else
+      render_unprocessable(note)
+    end
   end
 
   def show
+    note = Note.find_by_id(params[:id])
 
+    unless note.nil?
+      render json: note
+    else
+      render_unprocessable("Could not locate note.")
+    end
   end
 
   def index
+    notes = Note.all
 
+    render json: notes
   end
 
 
